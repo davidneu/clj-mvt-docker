bd093b6df48235bf9280f6e42f00cc9598a1ecef
David J. Neu <david@davidneu.com>
2019-08-15 07:52:52 -0400

    Remove unnecessary command.

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-cd /tmp
-
 docker_compose_version='1.24.1'
 
 sudo curl -L "https://github.com/docker/compose/releases/download/$docker_compose_version/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
